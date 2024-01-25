d956362d9585aef92ab78387275b8cd9a41436f0
myadang04 <mdang01@villanova.edu>
2024-01-25 13:46:10 -0500

    added files

---
+++ src/Pages/Home/Homescreen/index.jsx
@@ -0,0 +1,15 @@
+import HeroSection from "../HeroSection";
+import MySkills from "../MySkills";
+import MyExperience from "../MyExperience";
+import MyPortfolio from "../MyPortfolio";
+
+export default function Home() {
+    return (
+        <>
+            <HeroSection />
+            <MySkills />
+            <MyExperience />
+            <MyPortfolio />
+        </>
+    )
+}
---
+++ src/Pages/Home/MyExperience.jsx
@@ -0,0 +1,52 @@
+export default function MyExperience() {
+    return (
+        <section id="MyExperience" className="about--section">
+            <div className="hero--section--content--box about--section--box">
+                <div className="hero--section--content">
+                    <h1 className="skills-section--heading">My Experience</h1>
+                    <p className="hero--section-description">
+                        <h3>Undergraduate Research Assistant</h3>
+                        <h4>Villanova University</h4>
+                        <ul>
+                            <li>
+                                Conducting research with Dr. Justin DeBenedetto to develop the text generation of natural language processing systems
+                            </li>
+                            <li>
+                                Analyzing a dataset of 12,000 text samples to discern differences between computer-generated and human-generated text
+                            </li>
+                            <li>
+                                Training and improving the accuracy of the AMR classifier to detect whether a given sentence was human written or computer generated
+                            </li>
+                        </ul>
+                    </p>
+                    <p className="hero--section-description">
+                        <h3>Computer Administration Committee</h3>
+                        <h4>Special Olympics Pennsylvania</h4>
+                        <ul>
+                            <li>
+                                Developing the Villanova Special Olympics website, implementing new features to raise accessibility, and adding resources information for volunteers
+                            </li>
+                            <li>
+                                Managing the Special Olympics application and keeping volunteers and athletes updated about event schedule and competition information                            </li>
+                            <li>
+                                Monitoring athlete registration and other computer operations during Fall Festival 2023
+                            </li>
+                        </ul>
+                    </p>
+                    <p className="hero--section-description">
+                        <h3>Vice President</h3>
+                        <h4>Girls Who Code - Villanova University</h4>
+                        <ul>
+                            <li>
+                                Fostering an inclusive and empowering environment for young women interested in technology
+                            </li>
+                            <li>
+                                Hosting informative sessions and workshops to share career opportunities and resources and assist fellow students in developing coding and networking skills
+                            </li>
+                        </ul>
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
+}
